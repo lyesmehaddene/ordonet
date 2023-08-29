a2d532a63e934c6db2c1b0cdef5f7af6f14a53bb
Lyes Mehaddene <lyes.mehaddene@edhec.com>
2023-08-29 15:43:54 +0200

    working on controllers

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,22 +1,35 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointments = Appointment.find_by(patient: params[:patient_id])
   end
 
-  def show
-  end
+  def show; end
 
   def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.patient = get_patient_id
+    @appointment.doctor = current_user
+    if @appointment.save
+      redirect_to appointment_path(@appointment)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def new
+    @appointment = Appointment.new
   end
 
-  def update
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:first_name, :last_name)
   end
 
-  def edit
-  end
-
-  def destroy
+  def get_patient_id
+    @patient_first_name = params[:patient_first_name]
+    @patient_last_name = params[:patient_last_name]
+    @patient = Patient.find_by(first_name: @patient_first_name, last_name: @patient_last_name)
+    @patient.id
   end
 end
