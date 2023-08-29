7aedcdb1480f7be5f3f7e5e5eca2eedaa24d349a
Brice <108925232+Nikaiia@users.noreply.github.com>
2023-08-29 15:26:40 +0200

    seeds"

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,7 +1,6 @@
 class Medication < ApplicationRecord
 
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 
   has_many :ordo_medications
   has_many :ordonnances, through: :ordo_medications
