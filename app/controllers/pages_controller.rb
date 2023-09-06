9b0734082681eece5bd32f7ea17ef0fcb9d76513
Maria Coelho <mariiacoelho25@hotmail.com>
2023-09-06 12:58:34 +0200

    dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
-  def home; end
+  def home
+  end
 
-
-def dashboard
-end
+  def dashboard
+    @current_date = Date.today
+    @patients = current_user.doctor.patients.joins(:appointments)
+                .where('DATE(appointments.appointment_date) = ?', @current_date)
+                .distinct
+    @patient_count = @patients.count
+  end
 end
